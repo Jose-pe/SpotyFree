c853c90cec0f566539c0e75568aba83ab551200a
nzr_pe <joseluis.von@gmail.com>
2022-06-05 02:50:19 -0500

    e

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="css/bulma/css/bulma.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/mymain.css">
+    <link href="/css/bulma/css/bulma.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/mymain.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" integrity="sha512-BnbUDfEUfV0Slx6TunuB042k9tuKe3xrD6q4mg5Ed72LTgzDIcLPxg6yI2gcMFRyomt+yJJxE+zJwNmxki6/RA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Spotyfree</title>
 
